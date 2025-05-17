37297cb9aeb0890e1f3d395bf3304a189069d985
rippnitaa <145269418+Rifnita@users.noreply.github.com>
2025-05-18 03:25:49 +0700

    Delete matakuliah_api directory

--- matakuliah_api/routes.py
+++
@@ -1,6 +0,0 @@
-def includeme(config):
-    config.add_route('get_all', '/matakuliah')
-    config.add_route('create', '/matakuliah')
-    config.add_route('update', '/matakuliah/{id}')
-    config.add_route('delete', '/matakuliah/{id}')
-    config.scan()
